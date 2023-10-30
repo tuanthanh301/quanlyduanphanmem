fbdb45b1500a14588e81cacb4380bc85df9fc5dd
Nguyễn Tuấn Thành <116448857+tuanthanh301@users.noreply.github.com>
2023-10-30 21:14:01 +0700

    create-conflict

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         Đây là header
+    Tôi đang cố gắng tạo conflict để demo cho thầy Duy
       </header>
     </div>
   );
