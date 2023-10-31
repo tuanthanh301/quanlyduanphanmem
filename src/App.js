a1049a25babf2f1eab3e0978d34186551e448c1a
Nguyễn Tuấn Thành <116448857+tuanthanh301@users.noreply.github.com>
2023-10-31 14:09:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         Đây là header
     Tôi đang cố gắng tạo conflict để demo cho thầy Duy
+      Nhóm 7 môn quản lý dự án phần mềm
+        
       </header>
     </div>
   );
